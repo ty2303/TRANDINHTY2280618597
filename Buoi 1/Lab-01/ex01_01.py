670d441bcf07ac3261e40c66d1171011f7d86ab9
ty2303 <trandinhty23@gmail.com>
2025-02-26 15:43:05 +0700

    test

---
+++ Buoi 1/Lab-01/ex01_01.py
@@ -0,0 +1,3 @@
+ten = input("Nhap ten cua ban:")
+tuoi =input("Nhap tuoi cua ban :")
+print ( "chao mung ," , ten, "! Ban" , tuoi ,"tuoi.") 
---
+++ Buoi 1/Lab-01/ex02_02.py
@@ -0,0 +1,6 @@
+# Nhập bán kính từ người dùng
+bankinh = float(input("Nhập bán kính của hình tròn: "))
+# Tính diện tích của hình tròn
+dientich = 3.14 * (bankinh ** 2)
+# In diện tích của hình tròn ra màn hình
+print("Diện tích của hình tròn là:", dientich)
